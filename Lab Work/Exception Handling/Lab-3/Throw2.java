fe6faeb6622db85a4033daaa6c0381922efb864d
23DCS099 <23dcs099@charusat.edu.in>
2024-10-16 18:19:00 +0530

    added all the files

---
+++ Lab Work/Exception Handling/Lab-3/Throw2.java
@@ -0,0 +1,8 @@
+public class Throw2 {
+    public static void main(String[] args) {
+        System.out.println("User Defined Exception");
+        throw new ArithmeticException("Charusat/by zero");
+        // System.out.println("Rest of the code");
+        System.out.println("Name: Krishil Agrawal\nID: 23DCS001");
+    }
+}
